175d32f0a9a0c45bb4909c09e8a66fe30874c202
Farimah <108592778+farimah-tizghadam@users.noreply.github.com>
2025-08-07 14:08:52 +0330

    create website app
    create website app in order to render pages

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+context = {
+    'name' : 'Farimah',
+    'surname' : 'Tizghadam',
+    'email' : 'farimahtizghadam@gmail.com',
+    'phone' : '(+98)9106797104',
+    'Location' : 'Tehran, iran',
+}
+ 
+def index_view(request):
+    return render(request, 'index.html', context)
